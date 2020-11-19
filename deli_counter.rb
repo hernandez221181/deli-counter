8db000283b30e2f6de5833fd3bf6fe8ee7c7bf9f
Steven <hernandez221181@icloud.com>
2020-11-18 19:52:20 -0600

    commit tree aa151feeed3345adee9890ccf08230a274d99383

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+# let(:katz_deli) { [] }
+# let(:other_deli) { ["Logan", "Avi", "Spencer"] }
+# let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
+
+katz_deli = []
+
+def line (katz_deli)
+    if katz_deli.empty?
+        puts "The line is currently empty."
+    else
+        message = "The line is currently:"
+        katz_deli.each_with_index do |value, index|
+            message += "#{index.to_i+1}. #{value}"
+    end 
+end
+
+def take_a_number (katz_deli, name)
+    katz_deli.push(name)
+    spot = katz_deli.index(name) + 1
+    puts "Welcome, #{name}. You are number #{spot} in line."
+end
+
+def now_serving (katz_deli)
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        serving = katz_deli[0]
+        puts "Currently serving #{serving}."
+        katz_deli.shift
+    end
+end 
